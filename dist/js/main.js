e44bec02be12bcf32e3d0a30780cfa448484eb09
Kevin Toro <kctoro74@gmail.com>
2017-07-21 10:31:59 -0500

    Movimiento de Backgrounds

---
+++ dist/js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+    $(window).scroll(function(){
+        var barra = $(window).scrollTop();
+        var posicion = barra * 0.10;
+
+        $(".background").css({
+            "background-position": "0 -" + posicion + "px"
+        });
+    });
+});
