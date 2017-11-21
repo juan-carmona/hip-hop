1c3f1a3b8e869651e2895e50e3fba8e042dec105
Kevin Toro <kctoro74@gmail.com>
2017-11-21 16:55:48 -0500

    Realizadas las tareas especificadas en Trello

--- dev/js/02-main.js
+++ dev/js/02-main.js
@@ -2,3 +2,11 @@
     $('#carouselExampleIndicators').carousel({
       interval: false
     });
+
+$('body').bind('mousewheel', function(e) {
+    if(e.originalEvent.wheelDelta /120 > 0) {
+        $(this).carousel('prev');
+    } else {
+        $(this).carousel('next');
+    }
+});
