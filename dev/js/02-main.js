d735bf5295d89583b7c8215404615b84cab72e6a
Kevin Toro <kctoro74@gmail.com>
2017-11-26 22:41:08 -0500

    Implementación de backstretch
    Cambio de background automático

--- dev/js/02-main.js
+++ dev/js/02-main.js
@@ -1,10 +1,24 @@
 $(document).ready(function(){
         $(document).mousemove(function(ev){
-        	var x = -ev.clientX/50;
-        	var y = -ev.clientY/50;
+        	var x = ev.clientX/50;
+        	var y = ev.clientY/50;
 
-        	$(".background3").css({
-        		"backgroundPosition": x + "px " + y + "px"
-        	});
+        	$(".backstretch img").css({
+        		"left": -x + "px ",
+        		"margin-top": -y + "px",
+        		"right": x + "px ",
+        		"bottom": y + "px"
+        	},5000);
         });
+
+	$.backstretch([
+		"../img/city-1.jpg",
+		"../img/city-2.jpg",
+		"../img/city-3.jpg",
+		"../img/city-4.jpg",
+		"../img/city-5.jpg"
+	],{
+		fade:750,
+		duration:7000
+	});
 });
